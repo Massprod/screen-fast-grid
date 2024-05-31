31983e2ba05ef2a53dde4fc3ed2f3fe4c4d4bc30
Massprod <piannkovpe@gmail.com>
2024-05-31 11:49:27 +0500

    add descriptions and basic response model. fix response with status == 304, it shouldn't contain any content

---
+++ routers/wheels/models/response_models.py
@@ -0,0 +1,202 @@
+from pydantic import BaseModel
+from typing import ClassVar
+from fastapi import status as res_status
+
+
+find_response_examples = {
+    res_status.HTTP_302_FOUND: {
+        "description": "Wheel found",
+        "content": {
+            "application/json": {
+                "example": {
+                    "status": "SUCCESS",
+                    "message": "Wheel with ID: 12345 found.",
+                    "data": {
+                        "_id": "6658697150ffd9be65c4364d",
+                        "wheelId": "12345",
+                        "batchNumber": "54321",
+                        "wheelDiameter": 9999,
+                        "receiptDate": "2024-05-30T11:56:16.209000",
+                        "status": "laboratory",
+                        "wheelStack": None
+                    }
+                }
+            }
+        }
+    },
+    res_status.HTTP_404_NOT_FOUND: {
+        "description": "Wheel not found",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": {
+                        "status": "NOT_FOUND",
+                        "message": "Wheel with ID: 12345 not found.",
+                        "data": None
+                    }
+                }
+            }
+        }
+    },
+    res_status.HTTP_500_INTERNAL_SERVER_ERROR: {
+        "description": "Database error",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": "Database search error"
+                }
+            }
+        }
+    }
+}
+
+
+update_response_examples = {
+    res_status.HTTP_200_OK: {
+        "description": "Wheel updated successfully",
+        "content": {
+            "application/json": {
+                "example": {
+                    "status": "SUCCESS",
+                    "message": "Wheel with ID: 12345 has been updated successfully.",
+                    "data": {
+                        "wheelId": "12345",
+                        "batchNumber": "54321",
+                        "wheelDiameter": 9999,
+                        "receiptDate": "2024-05-30T11:56:16.209000",
+                        "status": "updated",
+                        "wheelStack": None
+                    }
+                }
+            }
+        }
+    },
+    res_status.HTTP_404_NOT_FOUND: {
+        "description": "Wheel not found",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": {
+                        "status": "NOT_FOUND",
+                        "message": "Wheel with ID: 12345 not found.",
+                        "data": None
+                    }
+                }
+            }
+        }
+    },
+    res_status.HTTP_304_NOT_MODIFIED: {
+        "description": "Wheel not modified",
+        "content": {
+            "application/json": {
+                "example": {
+                    "status": "NOT_MODIFIED",
+                    "message": "Wheel with ID: 12345 is already up to date.",
+                    "data": None
+                }
+            }
+        }
+    },
+    res_status.HTTP_500_INTERNAL_SERVER_ERROR: {
+        "description": "Database error",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": "Database update error"
+                }
+            }
+        }
+    }
+}
+
+
+create_response_examples = {
+    res_status.HTTP_201_CREATED: {
+        "description": "Wheel created successfully",
+        "content": {
+            "application/json": {
+                "example": {
+                    "status": "SUCCESS",
+                    "message": "Wheel with ID: W12345 has been created successfully.",
+                    "data": {
+                        "_id": "6658697150ffd9be65c4364d",
+                        "wheelId": "W12345",
+                        "batchNumber": "B54321",
+                        "wheelDiameter": 650,
+                        "receiptDate": "2024-05-30T11:56:16.209000+00:00",
+                        "status": "laboratory",
+                        "wheelStack": {
+                            "wheelStackId": "WS123",
+                            "wheelStackPosition": 2
+                        }
+                    }
+                }
+            }
+        }
+    },
+    res_status.HTTP_302_FOUND: {
+        "description": "Wheel already exists",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": "Wheel with provided `wheelId`=W12345, already exists",
+                }
+            }
+        }
+    },
+    res_status.HTTP_500_INTERNAL_SERVER_ERROR: {
+        "description": "Database error",
+        "content": {
+            "application/json": {
+                "example": {
+                    "detail": "Database insertion error"
+                }
+            }
+        }
+    }
+}
+
+
+class WheelsStandardResponse(BaseModel):
+    status: str = ""
+    message: str = ""
+    data: dict = None
+
+    STATUS_MAP: ClassVar[dict[int, str]] = {
+        res_status.HTTP_200_OK: "SUCCESS",
+        res_status.HTTP_201_CREATED: "CREATED",
+        res_status.HTTP_202_ACCEPTED: "ACCEPTED",
+        res_status.HTTP_204_NO_CONTENT: "NO_CONTENT",
+        res_status.HTTP_302_FOUND: "FOUND",
+        res_status.HTTP_304_NOT_MODIFIED: "NOT_MODIFIED",
+        res_status.HTTP_400_BAD_REQUEST: "VALIDATION_ERROR",
+        res_status.HTTP_401_UNAUTHORIZED: "UNAUTHORIZED",
+        res_status.HTTP_403_FORBIDDEN: "FORBIDDEN",
+        res_status.HTTP_404_NOT_FOUND: "NOT_FOUND",
+        res_status.HTTP_409_CONFLICT: "CONFLICT",
+        res_status.HTTP_500_INTERNAL_SERVER_ERROR: "ERROR"
+    }
+
+    def set_status(self, status_code: int):
+        self.status = self.STATUS_MAP.get(status_code, "UNKNOWN_STATUS")
+
+    def set_found_message(self, wheel_id: str):
+        self.message = f'Wheel with ID: {wheel_id} found.'
+
+    def set_not_found_message(self, wheel_id: str):
+        self.message = f'Wheel with ID: {wheel_id} not found.'
+
+    def set_create_message(self, wheel_id: str):
+        self.message = f'Wheel with ID: {wheel_id} has been created successfully.'
+
+    def set_duplicate_message(self, wheel_id: str):
+        self.message = f'Wheel with ID: {wheel_id} already exist in DB.'
+
+    def set_update_message(self, wheel_id: str):
+        self.message = f'Wheel with ID: {wheel_id} has been updated successfully.'
+
+    def set_up_to_date_message(self, wheel_id: str):
+        self.message = f'Wheel with ID: {wheel_id} already up to date.'
+
+    def set_delete_message(self, wheel_id: str):
+        self.message = f'Wheel with ID: {wheel_id} has been deleted successfully.'
--- routers/wheels/router.py
+++ routers/wheels/router.py
@@ -1,51 +1,166 @@
 from .crud import db_insert_wheel, db_find_wheel, make_json_friendly, db_update_wheel, db_delete_wheel
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status, Path, Body
 from fastapi.responses import JSONResponse, Response
 from database.mongo_connection import get_db
 from .models.models import CreateWheelRequest
+from .models.response_models import (WheelsStandardResponse,
+                                     update_response_examples,
+                                     find_response_examples,
+                                     create_response_examples)
 from motor.motor_asyncio import AsyncIOMotorClient
+from loguru import logger
 
 
 router = APIRouter()
 
 
-@router.put('/')
-async def update_wheel(
-        wheel: CreateWheelRequest,
+@router.get(
+    path='/{wheel_id}',
+    name='Find Wheel',
+    description="Retrieve the details of a wheel by it's ID",
+    response_class=JSONResponse,
+    status_code=status.HTTP_302_FOUND,
+    response_description='The details of the wheel, if found',
+    responses={
+        status.HTTP_302_FOUND: find_response_examples[status.HTTP_302_FOUND],
+        status.HTTP_404_NOT_FOUND: find_response_examples[status.HTTP_404_NOT_FOUND],
+    }
+)
+async def find_wheel(
+        wheel_id: str = Path(description="The ID of the wheel to retrieve", example="W12345"),
         db: AsyncIOMotorClient = Depends(get_db),
 ):
-    wheel_data = wheel.dict()
-    result = await db_update_wheel(db, wheel_data)
-    if result.matched_count == 0:
-        return Response(status_code=404)
-    elif result.modified_count == 0:
-        return Response(status_code=304)
-    return Response(status_code=200)
+    logger.info(f"Searching for a wheel with ID: {wheel_id}")
+    result = await db_find_wheel(db, wheel_id)
+    resp = WheelsStandardResponse()
+    status_code = status.HTTP_302_FOUND
+    if result is None:
+        logger.warning(f"Wheel with ID: {wheel_id} not found")
+        status_code = status.HTTP_404_NOT_FOUND
+        resp.set_status(status_code)
+        resp.set_not_found_message(wheel_id)
+        raise HTTPException(detail=resp.dict(), status_code=status_code)
+    logger.info(f"Wheel with ID: {wheel_id} found")
+    resp.set_status(status_code)
+    resp.set_found_message(wheel_id)
+    resp.data = await make_json_friendly(result)
+    return JSONResponse(content=resp.dict(), status_code=status_code)
 
 
-@router.post('/')
-async def create_wheel(
-        wheel: CreateWheelRequest,
+@router.put(
+    path='/',
+    name='Update Wheel',
+    description="Update the details of a wheel",
+    response_class=JSONResponse,
+    status_code=status.HTTP_200_OK,
+    response_description='The details of the wheel after update',
+    responses={
+        status.HTTP_200_OK: update_response_examples[status.HTTP_200_OK],
+        status.HTTP_404_NOT_FOUND: update_response_examples[status.HTTP_404_NOT_FOUND],
+        status.HTTP_304_NOT_MODIFIED: update_response_examples[status.HTTP_304_NOT_MODIFIED],
+    }
+)
+async def update_wheel(
+        wheel: CreateWheelRequest = Body(
+            ...,
+            description='Every parameter of the wheel is mandatory,'
+                        ' except the `wheelStack`.'
+                        ' It can be a `null` otherwise it should contain data of the existing `wheelStack` in a DB.',
+            example={
+                "wheelId": "W12345",
+                "batchNumber": "B54321",
+                "wheelDiameter": 650,
+                "receiptDate": "2024-05-30T11:56:16.209000+00:00",
+                "status": "laboratory",
+                "wheelStack": None
+            }
+        ),
         db: AsyncIOMotorClient = Depends(get_db),
 ):
     wheel_data = wheel.dict()
     wheel_id = wheel_data['wheelId']
+    logger.info(f"Updating wheel with ID: {wheel_id}")
+    result = await db_update_wheel(db, wheel_data)
+    resp = WheelsStandardResponse()
+    status_code: int = status.HTTP_200_OK
+    if result.matched_count == 0:
+        logger.warning(f"Wheel with ID: {wheel_id} not found")
+        status_code = status.HTTP_404_NOT_FOUND
+        resp.set_status(status_code)
+        resp.set_not_found_message(wheel_id)
+        raise HTTPException(detail=resp.dict(), status_code=status_code)
+    elif result.modified_count == 0:
+        logger.info(f"Wheel with ID: {wheel_id} is already up to date")
+        status_code = status.HTTP_304_NOT_MODIFIED
+        return Response(status_code=status_code)
+    logger.info(f"Wheel with ID: {wheel_id} has been updated")
+    resp.set_status(status_code)
+    resp.set_update_message(wheel_id)
+    return JSONResponse(content=resp.dict(), status_code=status_code)
+
+
+@router.post(
+    path='/',
+    description="Create New Wheel",
+    response_class=JSONResponse,
+    status_code=status.HTTP_201_CREATED,
+    response_description='The details of the newly created wheel',
+    responses={
+        status.HTTP_201_CREATED: create_response_examples[status.HTTP_201_CREATED],
+        status.HTTP_302_FOUND: create_response_examples[status.HTTP_302_FOUND],
+        status.HTTP_500_INTERNAL_SERVER_ERROR: create_response_examples[status.HTTP_500_INTERNAL_SERVER_ERROR],
+    }
+)
+async def create_wheel(
+        wheel: CreateWheelRequest = Body(
+            ...,
+            description='Every parameter of the wheel is mandatory,'
+                        ' except the `wheelStack`.'
+                        ' It can be a `null` otherwise it should contain data of the existing `wheelStack` in a DB.',
+            example={
+                "wheelId": "W12345",
+                "batchNumber": "B54321",
+                "wheelDiameter": 650,
+                "receiptDate": "2024-05-30T11:56:16.209000+00:00",
+                "status": "laboratory"
+            }
+        ),
+        db: AsyncIOMotorClient = Depends(get_db),
+):
+    wheel_data = wheel.dict()
+    wheel_id = wheel_data['wheelId']
+    resp = WheelsStandardResponse()
+    status_code: int = status.HTTP_201_CREATED
     result = await db_find_wheel(db, wheel_id)
     if result is not None:
-        return HTTPException(detail=f'Wheel with provided `wheelId`={wheel_id}, already exist',
-                             status_code=302)
+        logger.warning(f"Wheel with provided `wheelId`={wheel_id}, already exists")
+        status_code = status.HTTP_302_FOUND
+        resp.set_status(status_code)
+        resp.set_duplicate_message(wheel_id)
+        raise HTTPException(detail=resp.dict(), status_code=status_code)
     result = await db_insert_wheel(db, wheel_data)
     wheel_data['_id'] = result.inserted_id
-    return JSONResponse(content=await make_json_friendly(wheel_data), status_code=201)
+    resp.data = await make_json_friendly(wheel_data)
+    resp.set_status(status_code)
+    resp.set_create_message(wheel_id)
+    return JSONResponse(content=resp.dict(), status_code=status_code)
 
 
-@router.delete('/{wheel_id}')
+@router.delete(
+    path='/{wheel_id}',
+    name='Delete wheel',
+    description='Delete wheel from the DB, by its ID',
+    response_class=JSONResponse,
+    status_code=status.HTTP_200_OK,
+    response_description='Simple 200 for correct deletion, or 404 if wheel doesnt exist',
+)
 async def delete_wheel(
-        wheel_id: str,
+        wheel_id: str = Path(description='The ID of the wheel to delete', example='W12345'),
         db: AsyncIOMotorClient = Depends(get_db),
 ):
     result = await db_delete_wheel(db, wheel_id)
+    status_code: int = status.HTTP_200_OK
     if 0 == result.deleted_count:
-        return HTTPException(status_code=404,
-                             detail=f'Wheel with provided `wheelId`={wheel_id}, not found')
-    return Response(status_code=200)
+        status_code = status.HTTP_404_NOT_FOUND
+        return Response(status_code=status_code)
+    return Response(status_code=status_code)
