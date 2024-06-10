0a78e12552e3f2cdba5e210de299b51c7812a380
Massprod <piannkovpe@gmail.com>
2024-06-10 16:57:43 +0500

    add constant for more adequate reuse of values

---
+++ constants.py
@@ -0,0 +1,24 @@
+
+# Order options ENUM.
+ORDER_MOVE_WHOLE_STACK = 'moveWholeStack'
+ORDER_MOVE_TOP_WHEEL = 'moveTopWheel'
+ORDER_MOVE_TO_LABORATORY = 'moveToLaboratory'
+ORDER_MERGE_WHEEL_STACKS = 'mergeWheelstacks'
+
+# Order statuses
+ORDER_STATUS_IN_PROGRESS = 'inProgress'
+ORDER_STATUS_COMPLETED = 'completed'
+ORDER_STATUS_CANCELED = 'canceled'
+ORDER_STATUS_PENDING = 'pending'
+
+# DB names
+DB_PMK_NAME = 'pmkScreen'
+
+# DB Collections
+CLN_ACTIVE_ORDERS = 'activeOrders'
+CLN_BASE_PLACEMENT = 'basePlacement'
+CLN_CANCELED_ORDERS = 'canceledOrders'
+CLN_COMPLETED_ORDERS = 'completedOrders'
+CLN_GRID = 'grid'
+CLN_WHEEL_STACKS = 'wheelStacks'
+CLN_WHEELS = 'wheels'
