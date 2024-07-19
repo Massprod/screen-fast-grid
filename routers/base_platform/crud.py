8e41c6d1803329a9255fcaa4333e0db3d55a4cd3
Massprod <piannkovpe@gmail.com>
2024-07-19 15:14:30 +0500

    change of Placement endpoint. Now we allow placement of any existing wheelStack if placement cell is available. add force options to block|unblock|clear platform cells

--- routers/base_platform/crud.py
+++ routers/base_platform/crud.py
@@ -1,42 +1,336 @@
 from bson import ObjectId
+from loguru import logger
+from pymongo.errors import PyMongoError
+from fastapi import status, HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient
+from utility.utilities import get_db_collection, time_w_timezone, log_db_record
 
 
-async def platform_make_json_friendly(dict_to_convert):
-    dict_to_convert['_id'] = str(dict_to_convert['_id'])
-    dict_to_convert['createdAt'] = dict_to_convert['createdAt'].isoformat()
-    dict_to_convert['lastChange'] = dict_to_convert['lastChange'].isoformat()
-    for row in dict_to_convert['rows']:
-        for col in dict_to_convert['rows'][row]['columns']:
-            if dict_to_convert['rows'][row]['columns'][col]['wheelStack'] is None:
+async def platform_make_json_friendly(platform_data):
+    platform_data['_id'] = str(platform_data['_id'])
+    platform_data['preset'] = str(platform_data['preset'])
+    platform_data['createdAt'] = platform_data['createdAt'].isoformat()
+    platform_data['lastChange'] = platform_data['lastChange'].isoformat()
+    for row in platform_data['rows']:
+        for col in platform_data['rows'][row]['columns']:
+            if platform_data['rows'][row]['columns'][col]['wheelStack'] is None:
                 continue
-            dict_to_convert['rows'][row]['columns'][col]['wheelStack'] = str(
-                dict_to_convert['rows'][row]['columns'][col]['wheelStack']
+            platform_data['rows'][row]['columns'][col]['wheelStack'] = str(
+                platform_data['rows'][row]['columns'][col]['wheelStack']
             )
-    return dict_to_convert
+    if 'extra' in platform_data:
+        if 'orders' in platform_data:
+            orders = platform_data['extra']['orders']
+            for order in orders:
+                orders[order] = str(orders[order])
+    return platform_data
 
 
-async def get_platform(
+async def get_platform_by_object_id(
+        platform_object_id: ObjectId,
         db: AsyncIOMotorClient,
-        db_name: str = 'pmkScreen',
-        db_collection: str = 'basePlacement',
+        db_name: str,
+        db_collection: str,
 ):
-    collection = db[db_name][db_collection]
-    platform = await collection.find_one({'preset': 'pmkBase'})
-    return platform
+    collection = await get_db_collection(db, db_name, db_collection)
+    try:
+        platform = await collection.find_one({'_id': platform_object_id})
+        return platform
+    except PyMongoError as error:
+        logger.error(f'Error while searching in DB: {error}')
+        raise HTTPException(
+            detail=f'Error while searching in DB',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
-async def put_wheelstack_in_platform(
+async def get_platform_preset_by_object_id(
+        platform_object_id: ObjectId,
         db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    try:
+        preset_id = await collection.find_one(
+            {'_id': platform_object_id},
+            {'preset': 1},
+        )
+        return preset_id
+    except PyMongoError as error:
+        logger.error(f'Error while searching in DB: {error}')
+        raise HTTPException(
+            detail=f'Error while searching in DB',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def get_platform_by_name(
+        platform_name: str,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    try:
+        platform = await collection.find_one(
+            {'name': platform_name}
+        )
+        return platform
+    except PyMongoError as error:
+        logger.error(f'Error while searching in DB: {error}`')
+        raise HTTPException(
+            detail=f'Error while searching in DB',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def create_platform(
+        preset_data: dict,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    platform_data = {
+        'preset': preset_data['_id'],
+        'name': preset_data['name'],
+        'createdAt': await time_w_timezone(),
+        'lastChange': await time_w_timezone(),
+        'rowsOrder': preset_data['rowsOrder'],
+        'rows': preset_data['rows'],
+        'extra': preset_data['extra'],
+    }
+    collection = await get_db_collection(db, db_name, db_collection)
+    db_log_data = await log_db_record(db_name, db_collection)
+    logger.info(
+        f'Creating a new `platform` record in `{db_collection}` collection'
+        f' with `preset` = {preset_data['_id']}' + db_log_data
+    )
+    try:
+        res = await collection.insert_one(platform_data)
+        logger.info(
+            f'Successfully created a new `platform` with `objectId` = {res.inserted_id}' + db_log_data
+        )
+        return res
+    except PyMongoError as error:
+        logger.error(f'Error while creating `platform` = {error}' + db_log_data)
+        raise HTTPException(
+            detail=f'Error while creating `platform`',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def cell_exist(
+        platform_id,
+        row: str,
+        col: str,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    query = {
+        '_id': platform_id,
+        f'rows.{row}.columns.{col}.wheelStack': {
+            '$exists': True
+        }
+    }
+    try:
+        cell = await collection.find_one(query)
+        return cell
+    except PyMongoError as error:
+        raise HTTPException(
+            detail=f'Error while searching `cell` in {db_collection}: {error}',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def get_cell_status(
+        placement_id: ObjectId,
+        row: str,
+        col: str,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    query = {
+        f'_id': placement_id,
+        f'rows.{row}.columns.{col}.blocked': {
+            '$exists': True,
+        }
+    }
+    projection = {
+        f'rows.{row}.columns.{col}.blocked': 1,
+        '_id': 0,
+    }
+    try:
+        cell_status = await collection.find_one(query, projection)
+        return cell_status
+    except PyMongoError as error:
+        logger.error(f'Error while searching `cell_status` in {db_collection}: {error}')
+        raise HTTPException(
+            detail=f'Error while searching `cell_status`',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def get_cell_data(
+        platform_id: ObjectId,
+        row: str,
+        col: str,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    query = {
+        '_id': platform_id,
+        f'rows.{row}.columns.{col}.wheelStack': {
+            '$exists': True,
+        }
+    }
+    projection = {
+        f'rows.{row}.columns.{col}.wheelStack': 1,
+        '_id': 0,
+    }
+    try:
+        cell_data = await collection.find_one(query, projection)
+        return cell_data
+    except PyMongoError as error:
+        logger.error(f'Error while searching `cell_data` in {db_collection}: {error}')
+        raise HTTPException(
+            detail=f'Error while searching `cell_data`',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def place_wheelstack_in_platform(
+        placement_id: ObjectId,
+        wheelstack_object_id: ObjectId | None,
         row: str,
         column: str,
-        wheelstack_object_id: ObjectId | None,
-        db_name: str = 'pmkScreen',
-        db_collection: str = 'basePlacement',
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
 ):
-    collection = db[db_name][db_collection]
-    query_string: str = f'rows.{row}.columns.{column}.wheelStack'
-    document_filter = {query_string: {'$exists': True}}
-    update_data = {'$set': {query_string: wheelstack_object_id}}
-    result = await collection.update_one(document_filter, update_data)
-    return result
+    collection = await get_db_collection(db, db_name, db_collection)
+    query = {
+        '_id': placement_id,
+        f'rows.{row}.columns.{column}.wheelStack': {
+            '$exists': True
+        }
+    }
+    update = {
+        '$set': {
+            f'rows.{row}.columns.{column}.wheelStack': wheelstack_object_id,
+            'lastChange': await time_w_timezone(),
+        }
+    }
+    try:
+        result = await collection.update_one(query, update)
+        return result
+    except PyMongoError as error:
+        logger.error(f'Error while updating `cell_data` in {db_collection}: {error}')
+        raise HTTPException(
+            detail=f'Error while updating `cell_data',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+# BLock|Unblock|Clear cells
+async def block_platform_cell(
+        placement_id: ObjectId,
+        row: str,
+        column: str,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    query = {
+        '_id': placement_id,
+        f'rows.{row}.columns.{column}.blocked': {
+            '$exists': True
+        }
+    }
+    update = {
+        '$set': {
+            f'rows.{row}.columns.{column}.blocked': True,
+            'lastChange': await time_w_timezone(),
+        }
+    }
+    try:
+        result = await collection.update_one(query, update)
+        return result
+    except PyMongoError as error:
+        logger.error(f'Error while blocking `cell_data` in {db_collection}: {error}')
+        raise HTTPException(
+            detail=f'Error while blocking `cell_data',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def unblock_platform_cell(
+        placement_id: ObjectId,
+        row: str,
+        column: str,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    query = {
+        '_id': placement_id,
+        f'rows.{row}.columns.{column}.blocked': {
+            '$exists': True
+        }
+    }
+    update = {
+        '$set': {
+            f'rows.{row}.columns.{column}.blocked': False,
+            'lastChange': await time_w_timezone(),
+        }
+    }
+    try:
+        result = await collection.update_one(query, update)
+        return result
+    except PyMongoError as error:
+        logger.error(f'Error while unblocking `cell` {row}|{column} in {db_collection}: {error}')
+        raise HTTPException(
+            detail=f'Error while unblocking `cell_data',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def clear_platform_cell(
+        placement_id: ObjectId,
+        row: str,
+        column: str,
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    query = {
+        '_id': placement_id,
+        f'rows.{row}.columns.{column}': {
+            '$exists': True
+        }
+    }
+    update = {
+        '$set': {
+            f'rows.{row}.columns.{column}.wheelStack': None,
+            f'rows.{row}.columns.{column}.blocked': False,
+            'lastChange': await time_w_timezone(),
+        }
+    }
+    try:
+        result = await collection.update_one(query, update)
+        return result
+    except PyMongoError as error:
+        logger.error(f'Error while clearing `cell` {row}|{column} in {db_collection}: {error}')
+        raise HTTPException(
+            detail=f'Error while clearing `cell_data',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
--- routers/base_platform/router.py
+++ routers/base_platform/router.py
@@ -1,60 +1,301 @@
+from loguru import logger
 from fastapi import APIRouter, Depends, status, Query, Path, HTTPException, Response
 from fastapi.responses import JSONResponse
 from database.mongo_connection import mongo_client
-from .crud import get_platform, put_wheelstack_in_platform
+from .crud import get_platform_by_object_id, place_wheelstack_in_platform
 from bson import ObjectId
-from bson.errors import InvalidId
-from routers.wheelstacks.crud import db_find_wheelstack
-from routers.base_platform.crud import platform_make_json_friendly
-
+from motor.motor_asyncio import AsyncIOMotorClient
+from routers.presets.crud import get_preset_by_id
+from routers.grid.crud import collect_wheelstack_cells
+from routers.base_platform.crud import (platform_make_json_friendly,
+                                        get_platform_preset_by_object_id,
+                                        create_platform, get_platform_by_name,
+                                        block_platform_cell, unblock_platform_cell, clear_platform_cell)
+from utility.utilities import get_object_id
+from constants import DB_PMK_NAME, CLN_BASE_PLATFORM, CLN_PRESETS, PRES_TYPE_PLATFORM, CLN_WHEELSTACKS
+from routers.wheelstacks.crud import db_find_wheelstack_by_object_id
 
 router = APIRouter()
 
 
 @router.get(
-    path='/',
-    description='get current base platform state',
+    path='/{platform_object_id}',
+    description='get current platform state in DB by `objectId`',
+    response_class=JSONResponse,
+    name='Get platform state',
 )
-async def get_current_platform(
-        db=Depends(mongo_client.depend_client)
+async def route_get_platform_by_object_id(
+        platform_object_id: str = Path(..., description='`objectId` of stored `basePlatform`'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client)
 ):
-    res = await get_platform(db)
-    status_code = status.HTTP_200_OK
+    platform_id: ObjectId = await get_object_id(platform_object_id)
+    res = await get_platform_by_object_id(platform_id, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    if res is None:
+        raise HTTPException(
+            detail=f'`platform` with `objectId` = {platform_object_id} not Found.',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
     cor_res = await platform_make_json_friendly(res)
-    return JSONResponse(content=cor_res, status_code=status_code)
+    return JSONResponse(content=cor_res, status_code=status.HTTP_200_OK)
+
+
+@router.get(
+    path='/name/{name}',
+    description='get current `basePlatform` state in DB by `name`',
+    response_class=JSONResponse,
+    name='Get Platform state'
+)
+async def route_get_platform_by_name(
+        name: str = Path(...,
+                         description='`name` of stored `grid`'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+):
+    exist = await get_platform_by_name(name, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    if exist is None:
+        raise HTTPException(
+            detail=f'`basePlatform` with `name` = {name}. Not Found.',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    cor_res = await platform_make_json_friendly(exist)
+    return JSONResponse(content=cor_res, status_code=status.HTTP_200_OK)
+
+
+@router.get(
+    path='/preset/{platform_object_id}',
+    description='get `objectId` of used `preset` to build the `grid`',
+    response_class=JSONResponse,
+    name='Get used `preset'
+)
+async def route_get_platform_preset_by_object_id(
+        platform_object_id: str = Path(
+            ...,
+            description='`objectId` of the `platform` for which we need to get used `preset`',
+        ),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+):
+    logger.info(f'Receiver request for a `platform` with `objectId` = {platform_object_id}')
+    platform_id: ObjectId = await get_object_id(platform_object_id)
+    res = await get_platform_preset_by_object_id(platform_id, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    if res is None:
+        logger.warning(f'`platform` with `objectId` = {platform_object_id}. Not found')
+        raise HTTPException(
+            detail=f'`platform` with `objectId` = {platform_object_id}. Not found',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    preset_id = str(res['preset'])
+    logger.info(
+        f'Successfully found `preset` `objectId` for `platform` = `{platform_object_id}'
+        f' Returning `preset` `objectId` {platform_object_id}'
+    )
+    return JSONResponse(
+        content={
+            'preset_id': preset_id,
+        },
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.post(
+    path='/create/{preset_object_id}',
+    description='Creating an empty `platform` accordingly with provided `preset`',
+    response_class=JSONResponse
+)
+async def route_create_empty_platform(
+        preset_object_id: str = Path(..., description='`objectId` of the `preset` to use'),
+        name: str = Query(...,
+                          description='unique name of `basePlatform` to use'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+):
+    logger.info(f'Receiver request to create a new `platform` by `objectId` of a `preset` = {preset_object_id}')
+    cor_name: str = name.strip()
+    if not cor_name:
+        raise HTTPException(
+            detail=f"Query parameter `grid_name` shouldn't be empty",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+    preset_id: ObjectId = await get_object_id(preset_object_id)
+    preset_data = await get_preset_by_id(preset_id, db, DB_PMK_NAME, CLN_PRESETS)
+    if preset_data is None:
+        raise HTTPException(
+            detail=f'Preset with provided `objectID` = `{preset_object_id}`. Not Found.',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    if preset_data['presetType'] != PRES_TYPE_PLATFORM:
+        raise HTTPException(
+            detail=f'Incorrect preset type, only `{PRES_TYPE_PLATFORM}` is allowed.',
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+    exist = await get_platform_by_name(cor_name, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    if exist:
+        raise HTTPException(
+            detail=f'`basePlatform` with such name already exist',
+            status_code=status.HTTP_302_FOUND,
+        )
+    correct_data = await collect_wheelstack_cells(preset_data)
+    correct_data['name'] = cor_name
+    res = await create_platform(correct_data, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    logger.info(
+        f'Successfully created a `platform` from `preset` with `objectId` = {preset_object_id}.'
+        f' Returning a new `platform` `objectId` = {res.inserted_id}'
+    )
+    return JSONResponse(
+        content={
+            'object_id': str(res.inserted_id),
+        },
+        status_code=status.HTTP_200_OK,
+    )
 
 
 @router.put(
-    path='/{wheelstack_object_id}',
-    description='wheelstack object id ',
+    path='/place/{platform_object_id}/{wheelstack_object_id}',
+    description="`WARNING`"
+                "\n Force placement on some cell without any dependencies."
+                "\n We're just placing `objectId` of the given `wheelStack` in chosen cell."
+                "\n If it's empty, otherwise reject."
+                "\n Without any dependency changes.",
+    name='Force Cell Placement',
 )
-async def move_wheelstack_in_the_platform(
-        row: str = Query(...),
-        column: str = Query(...),
-        wheelstack_object_id: str = Path(...),
-        db=Depends(mongo_client.depend_client),
+async def route_force_place_wheelstack_in_the_platform(
+        platform_object_id: str = Path(...,
+                                       description="`objectId` of the `platform` to place into"),
+        wheelstack_object_id: str = Path(...,
+                                         description='`objectId` of the `wheelStacks` to place'),
+        row: str = Query(...,
+                         description='`row` of the cell placement'),
+        column: str = Query(...,
+                            description='`column` of the cell placement'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
 ):
-    status_code: int = status.HTTP_200_OK
-    try:
-        object_id = ObjectId(wheelstack_object_id)
-    except InvalidId as e:
-        status_code = status.HTTP_400_BAD_REQUEST
-        raise HTTPException(detail=str(e), status_code=status_code)
-    exist = await db_find_wheelstack(db, object_id)
+    # There's only 2 checks we need to check even with our FORCE method.
+    # Because, we shouldn't be able to place on non-existing cells
+    #  some non-existing wheelstacks.
+    # And if order already blocks placement, or it's already taken by other `wheelStack`.
+    # Otherwise, we're just placing `wheelStack` on this cell.
+    # Again, without any dependencies, because everything should be done in ORDERS.
+    # This is like HAND-Control, and foundation to use with extra functions.
+    platform_id: ObjectId = await get_object_id(platform_object_id)
+    wheelstack_id: ObjectId = await get_object_id(wheelstack_object_id)
+    exist = await db_find_wheelstack_by_object_id(wheelstack_id, db, DB_PMK_NAME, CLN_WHEELSTACKS)
     if exist is None:
-        status_code = status.HTTP_404_NOT_FOUND
-        raise HTTPException(detail='Wheelstack doesnt exist', status_code=status_code)
+        raise HTTPException(
+            detail='Wheelstack doesnt exist',
+            status_code=status.HTTP_404_NOT_FOUND
+        )
     if exist['blocked']:
-        status_code = status.HTTP_403_FORBIDDEN
-        raise HTTPException(detail='Wheelstack blocked', status_code=status_code)
-
-    res = await put_wheelstack_in_platform(
-        db, row, column, object_id,
+        raise HTTPException(
+            detail='Wheelstack blocked',
+            status_code=status.HTTP_403_FORBIDDEN
+        )
+    platform = await get_platform_by_object_id(platform_id, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    # We can take it from DB, but we already have taken data for the `platform`.
+    # And we can ignore all the data and leave only `_id` of the platform.
+    # But I think it's just simpler to check what we already got, without any extra DB calls.
+    try:
+        cell = platform['rows'][row]['columns'][column]
+    except KeyError as error:
+        logger.error(f'Attempt to force placement on non existing cell: `row` = {row} | `column` = {column}')
+        raise HTTPException(
+            detail=f"{row}|{column} cell doesnt exist",
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    if cell['blocked']:
+        raise HTTPException(
+            detail='Cell is blocked',
+            status_code=status.HTTP_403_FORBIDDEN,
+        )
+    if cell['wheelStack'] is not None:
+        stored = cell['wheelStack']
+        # Already placed.
+        if stored == wheelstack_id:
+            return Response(status_code=status.HTTP_200_OK)
+        raise HTTPException(
+            detail='Cell is already taken',
+            status_code=status.HTTP_403_FORBIDDEN
+        )
+    res = await place_wheelstack_in_platform(
+        platform_id, wheelstack_id, row, column, db, DB_PMK_NAME, CLN_BASE_PLATFORM
     )
+    # Leaving it as extra check. Actually don't need it, we already know placement exists or not.
     if 0 == res.matched_count:
-        status_code = status.HTTP_404_NOT_FOUND
-        raise HTTPException(detail='Placement doesnt exist', status_code=status_code)
+        raise HTTPException(
+            detail='Placement doesnt exist',
+            status_code=status.HTTP_404_NOT_FOUND
+        )
     if 0 == res.modified_count:
         status_code = status.HTTP_304_NOT_MODIFIED
         raise HTTPException(detail='Not modified', status_code=status_code)
-    return Response(status_code=status_code)
+    return Response(status_code=status.HTTP_200_OK)
+
+
+# BLock|Unblock|Clear cells
+@router.put(
+    path='/block/{platform_object_id}',
+    description='Force block state on a chosen cell',
+    name='Force Block',
+)
+async def route_force_block_of_cell(
+        row: str = Query(...,
+                         description='`row` of desired cell'),
+        column: str = Query(...,
+                            description='`column` of desired cell'),
+        platform_object_id: str = Path(...,
+                                       description='`objectId` of the placement'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client)
+):
+    platform_id: ObjectId = await get_object_id(platform_object_id)
+    res = await block_platform_cell(platform_id, row, column, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    if 0 == res.matched_count:
+        raise HTTPException(
+            detail=f"{row}|{column} Cell or placement with given `objectId` = `{platform_object_id}` doesn't exist.",
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+    return Response(status_code=status.HTTP_200_OK)
+
+
+@router.put(
+    path='/unblock/{platform_object_id}',
+    description='Force unblock status on a chosen cell',
+    name='Force Unblock',
+)
+async def route_force_unblock_of_cell(
+        row: str = Query(...,
+                         description='`row` of desired cell'),
+        column: str = Query(...,
+                            description='`column` of desired cell'),
+        platform_object_id: str = Path(...,
+                                       description='`objectId` of the placement'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+):
+    platform_id: ObjectId = await get_object_id(platform_object_id)
+    res = await unblock_platform_cell(platform_id, row, column, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    if 0 == res.matched_count:
+        raise HTTPException(
+            detail=f"{row}|{column} Cell or placement with given `objectId` = `{platform_object_id}` doesn't exist.",
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+    return Response(status_code=status.HTTP_200_OK)
+
+
+@router.put(
+    path='/clear/{platform_object_id}',
+    description='Force clearing on a chosen cell. Making it empty and unblocked.',
+    name='Force Cell Clear',
+)
+async def route_force_clear_of_cell(
+        row: str = Query(...,
+                         description='`row` of desired cell'),
+        column: str = Query(...,
+                            description='`column` of desired cell'),
+        platform_object_id: str = Path(...,
+                                       description='`objectId` of the placement'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+):
+    platform_id: ObjectId = await get_object_id(platform_object_id)
+    res = await clear_platform_cell(platform_id, row, column, db, DB_PMK_NAME, CLN_BASE_PLATFORM)
+    if 0 == res.matched_count:
+        raise HTTPException(
+            detail=f"{row}|{column} Cell or placement with given `objectId` = `{platform_object_id}` doesn't exist.",
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+    return Response(status_code=status.HTTP_200_OK)
+
