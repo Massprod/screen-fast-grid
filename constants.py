9f1c2a463186219946fe8c80e952c54cff5ddf12
Massprod <piannkovpe@gmail.com>
2024-07-19 11:03:10 +0500

    add new constant project values

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ ORDER_MOVE_TOP_WHEEL = 'moveTopWheel'
 ORDER_MOVE_TO_LABORATORY = 'moveToLaboratory'
 ORDER_MERGE_WHEEL_STACKS = 'mergeWheelstacks'
 
-# Order statuses
+# Order statuses ENUM.
 ORDER_STATUS_IN_PROGRESS = 'inProgress'
 ORDER_STATUS_COMPLETED = 'completed'
 ORDER_STATUS_CANCELED = 'canceled'
@@ -16,9 +16,35 @@ DB_PMK_NAME = 'pmkScreen'
 
 # DB Collections
 CLN_ACTIVE_ORDERS = 'activeOrders'
-CLN_BASE_PLACEMENT = 'basePlacement'
+CLN_BASE_PLATFORM = 'basePlatform'
 CLN_CANCELED_ORDERS = 'canceledOrders'
 CLN_COMPLETED_ORDERS = 'completedOrders'
 CLN_GRID = 'grid'
-CLN_WHEEL_STACKS = 'wheelStacks'
+CLN_WHEELSTACKS = 'wheelStacks'
 CLN_WHEELS = 'wheels'
+CLN_PRESETS = 'presets'
+# PRESETS
+PRES_PMK_GRID = 'pmkGrid'
+PRES_PMK_PLATFORM = 'pmkBasePlatform'
+
+# PRESET TYPES
+PRES_TYPE_GRID = 'grid'
+PRES_TYPE_PLATFORM = 'basePlatform'
+
+# EXTRA ELEMENT TYPES
+EE_HAND_CRANE = 'handCrane'
+
+# FOLDERS
+FLD_BASIC_SCHEMAS = 'database/collections/schemas'
+
+# PLACEMENT STATUSES
+PS_LABORATORY = 'laboratory'
+PS_SHIPPED = 'shipped'
+PS_GRID = 'grid'
+PS_BASE_PLATFORM = 'basePlatform'
+PS_REJECTED = 'rejected'
+
+
+# WHEELSTACKS LIMIT
+WS_MIN_WHEELS = 0
+WS_MAX_WHEELS = 6
