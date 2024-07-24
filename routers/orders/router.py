311089348c1faf2d540526b2c69dabcae8dd47dd
Massprod <piannkovpe@gmail.com>
2024-07-24 15:27:48 +0500

    add orders cancellation endpoint

--- routers/orders/router.py
+++ routers/orders/router.py
@@ -2,6 +2,8 @@ from bson import ObjectId
 from database.mongo_connection import mongo_client
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.responses import JSONResponse, Response
+from routers.orders.crud import db_find_order_by_object_id
+from routers.orders.orders_cancelation import orders_cancel_move_to_laboratory
 from routers.orders.models.models import CreateMoveOrderRequest, CreateLabOrderRequest, CreateProcessingOrderRequest
 from routers.orders.orders_creation import (orders_create_move_whole_wheelstack,
                                             orders_create_move_to_laboratory,
@@ -9,15 +11,10 @@ from routers.orders.orders_creation import (orders_create_move_whole_wheelstack,
                                             orders_create_move_to_rejected,
                                             )
 from fastapi import APIRouter, Depends, HTTPException, status, Body, Path, Query
-from constants import (ORDER_MOVE_WHOLE_STACK, ORDER_MOVE_TO_LABORATORY, ORDER_MOVE_TO_PROCESSING,
-                       PRES_TYPE_GRID, ORDER_MOVE_TO_REJECTED,
-                       PRES_TYPE_PLATFORM, DB_PMK_NAME, CLN_GRID,
-                       CLN_WHEELSTACKS, CLN_BASE_PLATFORM, ORDER_STATUS_PENDING, CLN_ACTIVE_ORDERS)
-from routers.grid.crud import db_get_grid_cell_data, db_update_grid_cell_data
-from routers.base_platform.crud import db_get_platform_cell_data, db_update_platform_cell_data
-from routers.wheelstacks.crud import db_find_wheelstack_by_object_id, db_update_wheelstack
-from routers.orders.crud import db_create_order
-from utility.utilities import get_object_id, time_w_timezone
+from constants import (ORDER_MOVE_WHOLE_STACK, ORDER_MOVE_TO_LABORATORY,
+                       ORDER_MOVE_TO_PROCESSING, ORDER_MOVE_TO_REJECTED,
+                       DB_PMK_NAME, CLN_ACTIVE_ORDERS)
+from utility.utilities import get_object_id
 from loguru import logger
 
 router = APIRouter()
@@ -41,7 +38,7 @@ router = APIRouter()
 async def route_post_create_order(
         order_data: CreateMoveOrderRequest = Body(...,
                                                   description='all required data for a new `order`'),
-        db: AsyncIOMotorClient = Depends(mongo_client.get_client),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
 ):
     # TODO: `moveTopWheel` and `mergeWheelStacks` are extra orders with is not required.
     #   but it's going to be a good practice and useful to do.
@@ -66,7 +63,7 @@ async def route_post_create_order(
 async def route_post_create_order_move_to_lab(
         order_data: CreateLabOrderRequest = Body(...,
                                                  description='all required data for a new lab `order`'),
-        db: AsyncIOMotorClient = Depends(mongo_client.get_client),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
 ):
     data = order_data.model_dump()
     logger.info(f'Creating order of type = `{ORDER_MOVE_TO_LABORATORY}`')
@@ -87,7 +84,7 @@ async def route_post_create_order_move_to_lab(
 async def route_post_create_order_move_to_processing(
         order_data: CreateProcessingOrderRequest = Body(...,
                                                         description='all required data for a new processing `order`'),
-        db: AsyncIOMotorClient = Depends(mongo_client.get_client)
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client)
 ):
     data = order_data.model_dump()
     logger.info(f'Creating order of type = {ORDER_MOVE_TO_PROCESSING}')
@@ -109,7 +106,7 @@ async def route_post_create_order_move_to_rejected(
         # TODO: If we change logic for this order, change MODEL
         order_data: CreateProcessingOrderRequest = Body(...,
                                                         description='all required data for a new processing `order`'),
-        db: AsyncIOMotorClient = Depends(mongo_client.get_client),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
 ):
     data = order_data.model_dump()
     logger.info(f'Creating order of type = {ORDER_MOVE_TO_REJECTED}')
@@ -120,3 +117,30 @@ async def route_post_create_order_move_to_rejected(
         },
         status_code=status.HTTP_201_CREATED,
     )
+
+
+@router.delete(
+    path='/cancel/{order_object_id}',
+    description=f'Cancels existing order',
+    name='Cancel Order',
+)
+async def route_delete_cancel_order(
+        order_object_id: str = Path(...,
+                                    description='`objectId` of the order to cancel'),
+        cancellation_reason: str = Query('',
+                                         description='reason of cancellation'),
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+):
+    order_id: ObjectId = await get_object_id(order_object_id)
+    order_data = await db_find_order_by_object_id(order_id, db, DB_PMK_NAME, CLN_ACTIVE_ORDERS)
+    if order_data is None:
+        raise HTTPException(
+            detail=f'Order with `objectId` = {order_id}. Not Found.',
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+    if ORDER_MOVE_TO_LABORATORY == order_data['orderType']:
+        result = await orders_cancel_move_to_laboratory(order_data, cancellation_reason, db)
+        logger.info(f'Order canceled and move to `canceledOrders` with `_id` = {result}')
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
