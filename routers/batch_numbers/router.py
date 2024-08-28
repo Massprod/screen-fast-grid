ad72f71ad214eafa1d76be956e40b0eaddafc036
Massprod <piankovpe@gmail.com>
2024-08-28 13:35:16 +0500

    add endpoint for getting data of `batchNumber`s in period

--- routers/batch_numbers/router.py
+++ routers/batch_numbers/router.py
@@ -2,10 +2,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from database.mongo_connection import mongo_client
 from fastapi.responses import JSONResponse, Response
 from fastapi import APIRouter, Depends, HTTPException, status, Path, Query
+
+from utility.utilities import get_correct_datetime
 from .crud import (db_find_batch_number,
                    db_find_all_batch_numbers,
                    db_change_lab_status,
-                   batch_number_record_make_json_friendly
+                   batch_number_record_make_json_friendly, db_find_all_batch_numbers_in_period
                    )
 from constants import DB_PMK_NAME, CLN_BATCH_NUMBERS
 
@@ -33,6 +35,43 @@ async def route_get_all_batch_numbers(
         resp_data[record['batchNumber']] = await batch_number_record_make_json_friendly(record)
     return JSONResponse(
         content=resp_data,
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.get(
+    path='/period',
+    name='Get In Period',
+    description='Return all of the `batch_number` in the provided period range.'
+                ' Range is from first_day -> target_day, inclusive.'
+                ' Format: YYYY-mm-dd',
+)
+async def route_get_all_batch_numbers_in_period(
+        db: AsyncIOMotorClient = Depends(mongo_client.depend_client),
+        period_start: str = Query(...,
+                                  description='Start of the period in format: `YYYY-mm-dd`'),
+        period_end: str = Query(...,
+                                description='End of the period in format: `YYYY-mm-dd`')
+):
+    correct_start = await get_correct_datetime(period_start)
+    # TODO: Move this to Model, but later.
+    if not correct_start:
+        raise HTTPException(
+            detail='Incorrect `period_start` format. Correct: `YYYY-mm-dd`',
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+    correct_end = await get_correct_datetime(period_end)
+    if not correct_end:
+        raise HTTPException(
+            detail='Incorrect `period_end` format. Correct: `YYYY-mm-dd`',
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+    resp = await db_find_all_batch_numbers_in_period(
+        correct_start, correct_end, db, DB_PMK_NAME, CLN_BATCH_NUMBERS
+    )
+    cor_resp = [await batch_number_record_make_json_friendly(record) for record in resp]
+    return JSONResponse(
+        content=cor_resp,
         status_code=status.HTTP_200_OK,
     )
 
