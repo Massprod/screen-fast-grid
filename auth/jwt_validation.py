0c9b4ab9a2bef99d940c82807f648dbcd4bb28e6
Massprod <piankovpe@gmail.com>
2024-10-03 10:05:06 +0500

    add option to disable JWT validation

--- auth/jwt_validation.py
+++ auth/jwt_validation.py
@@ -1,19 +1,31 @@
+import os
+import dotenv
 from loguru import logger
-from functools import partial
+from typing import Optional
 from jose import jwt, JWTError
 from starlette.status import HTTP_401_UNAUTHORIZED
 from fastapi import Depends, HTTPException, status
+from constants import PUBLIC_KEY, ALGORITHM, ISSUER
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from constants import PUBLIC_KEY, ALGORITHM, ISSUER, ADMIN_ROLE, MANAGER_ROLE, LAB_PERSONAL_ROLE
 
 
-oauth_security = HTTPBearer()  # Specify your token URL
+if not dotenv.load_dotenv('.env'):
+    logger.warning('Failed to load .env file, using system environment variables.')
+valid_token_required: bool = os.getenv('JWT_VALIDATION_TOKEN_REQ', 'false').lower() == 'true'
+logger.warning(
+    f'Setting for JWT validation required: {valid_token_required}'
+)
+oauth_security = HTTPBearer(
+    auto_error=valid_token_required
+)
 issuerName: str = ISSUER
 
 
 async def validate_credentials(
         credentials: HTTPAuthorizationCredentials = Depends(oauth_security)
 ) -> dict | None:
+    if credentials is None and not valid_token_required:
+        return None
     token: str = credentials.credentials
     logger.info(
         f'Validating access token: {token}'
@@ -62,6 +74,8 @@ async def verify_multi_roles_token(
         allowed_roles: set[str],
         credentials: HTTPAuthorizationCredentials = Depends(oauth_security),
 ) -> dict | None:
+    if credentials is None and not valid_token_required:
+        return None
     token: str = credentials.credentials
     allowed_roles_log: str = ' | '.join(allowed_roles)
     logger.info(
@@ -93,6 +107,6 @@ async def verify_multi_roles_token(
 
 
 def get_role_verification_dependency(allowed_roles: set[str]) -> callable:
-    async def dependency(credentials: HTTPAuthorizationCredentials = Depends(oauth_security)):
+    async def dependency(credentials: Optional[HTTPAuthorizationCredentials] = Depends(oauth_security)):
         return await verify_multi_roles_token(allowed_roles=allowed_roles, credentials=credentials)
     return Depends(dependency)
