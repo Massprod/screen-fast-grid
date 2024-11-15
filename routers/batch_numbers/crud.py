d68ce8e2457ac25199d2c3b912f4591f1a0d3cfd
Massprod <piankovpe@gmail.com>
2024-11-15 13:20:47 +0500

    add option to get `batchNumber`s with `unplaced` wheels

--- routers/batch_numbers/crud.py
+++ routers/batch_numbers/crud.py
@@ -2,7 +2,7 @@ from loguru import logger
 from pymongo.errors import PyMongoError, DuplicateKeyError
 from fastapi import status, HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorClientSession
-from utility.utilities import get_db_collection, log_db_record, time_w_timezone
+from utility.utilities import get_db_collection, log_db_error_record, log_db_record, time_w_timezone
 from datetime import timedelta, datetime
 
 
@@ -27,32 +27,12 @@ async def db_create_batch_number(
     logger.info(
         f'Creating a new `batchNumber` record in `{db_collection}` collection' + db_log_data
     )
-    # max_retries = 6
-    #
-    # for attempt in range(max_retries):
     try:
         res = await collection.insert_one(batch_number_data, session=session)
         logger.info(
             f'Successfully created a new `batchNumber` with `objectId` = {res.inserted_id}' + db_log_data
         )
         return res
-    # We don't actually care about creating it at withing transaction,
-    #  so if record exists => Ignore.
-    # And we're making multiple async requests, the reason why it fails.
-    # except (pymongo.errors.ConnectionFailure, pymongo.errors.OperationFailure) as error:
-    #     if error.has_error_label("TransientTransactionError"):
-    #         logger.warning(f"TransientTransactionError: {error}. Attempt {attempt + 1} of {max_retries}")
-    #         if attempt < max_retries - 1:
-    #             await asyncio.sleep(1)
-    #         else:
-    #             logger.error(f"Max retries reached. Transaction aborted: {error}")
-    #             return
-    #     else:
-    #         logger.error(f'Error while creating `batchNumber`: {error}' + db_log_data)
-    #         raise HTTPException(
-    #             detail=f'Error while creating `batchNumber`',
-    #             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-    #         )
     except DuplicateKeyError as error:
         logger.warning(f'Error while creating `batchNumber`, already exists: {error}')
         return
@@ -198,6 +178,50 @@ async def db_find_all_batch_numbers_in_period(
         logger.error(
             f'Error while searching `batchNumber` within period: start = {period_start} -> end = {period_end}'
             + db_log_data + f'Error: {error}'
+        )
+        raise HTTPException(
+            detail='Error while searching `batchNumber` records',
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+async def db_find_batch_numbers_w_unplaced(
+        db: AsyncIOMotorClient,
+        db_name: str,
+        db_collection: str,
+        session: AsyncIOMotorClientSession = None,
+):
+    collection = await get_db_collection(db, db_name, db_collection)
+    db_log_data = await log_db_record(db_name, db_collection)
+    lookup = {
+        '$lookup': {
+            'from': 'wheels',
+            'localField': 'batchNumber',
+            'foreignField': 'batchNumber',
+            'as': 'matchedWheels',
+        }
+    }
+    match = {
+        '$match': {
+            'matchedWheels.status': 'unplaced',
+        }
+    }
+    project = {
+        '$project': {
+            'batchNumber': 1,
+            "_id": 0
+        }
+    }
+    logger.info(
+        f'Searching all `batchNumber`s with unplaced wheels' + db_log_data
+    )
+    try:
+        res = await collection.aggregate([lookup, match, project], session=session).to_list(length=None)
+        return res
+    except PyMongoError as error:
+        error_log: str = await log_db_error_record(error)
+        logger.error(
+            f'Error while searching `batchNumber`s with unplaced wheels' + error_log + db_log_data
         )
         raise HTTPException(
             detail='Error while searching `batchNumber` records',
